5fe4b45296d59818663dbddbb8955e92db046e23
MINSIX <98219599+MINSIX@users.noreply.github.com>
2024-03-28 15:13:14 +0900

    한줄에 하나씩
    #유튜브 섬네일이나 컨퍼런스이름#이부분을 오른쪽에 둿으면좋겟어요

--- src/pages/searchResult/[id].tsx
+++ src/pages/searchResult/[id].tsx
@@ -92,7 +92,7 @@ const searchResultId = () => {
           </NavigationMenu>
           </section>
                 
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-4">
     
       {results.length > 0 ? (
         results.map(result => (
